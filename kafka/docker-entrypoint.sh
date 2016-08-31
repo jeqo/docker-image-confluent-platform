0af1e761fd4391b9a62979665971dc1119059317
Jorge Quilcate <quilcate.jorge@gmail.com>
2016-08-31 04:15:03 +0200

    kubernetes config

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 echo "zookeeper: $ZOOKEEPER_HOST:$ZOOKEEPER_PORT"
 sed -i "s/^\(zookeeper\.connect\s*=\s*\).*\$/\1$ZOOKEEPER_HOST\:$ZOOKEEPER_PORT/" etc/kafka/server.properties
 
-while ! nc -z $ZOOKEEPER_HOST $ZOOKEEPER_PORT;
-do sleep 0.1 && echo 'zookeeper down!';
-done;
-echo 'zookeeper is up!'
+#while ! nc -z $ZOOKEEPER_HOST $ZOOKEEPER_PORT;
+#do sleep 0.1 && echo 'zookeeper down!';
+#done;
+#echo 'zookeeper is up!'
 
 exec "$@"
--- schema-registry/docker-entrypoint.sh
+++ schema-registry/docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 echo "zookeeper: $ZOOKEEPER_HOST:$ZOOKEEPER_PORT"
 sed -i "s/^\(kafkastore\.connection\.url\s*=\s*\).*\$/\1$ZOOKEEPER_HOST\:$ZOOKEEPER_PORT/" etc/schema-registry/schema-registry.properties
 
-while ! nc -z $ZOOKEEPER_HOST $ZOOKEEPER_PORT;
-do sleep 0.1 && echo 'zookeeper down!';
-done;
-echo 'zookeeper is up!'
+#while ! nc -z $ZOOKEEPER_HOST $ZOOKEEPER_PORT;
+#do sleep 0.1 && echo 'zookeeper down!';
+#done;
+#echo 'zookeeper is up!'
 
 exec "$@"
